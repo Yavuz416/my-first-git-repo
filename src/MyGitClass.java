89fa88743b92e9107636151b70dfa3f2b679223c
Yavuz Kuran <yavuz.kuran1@gmail.com>
2022-08-03 19:24:13 -0400

    third commit

--- src/MyGitClass.java
+++ src/MyGitClass.java
@@ -1,6 +1,10 @@
 public class MyGitClass {
     public static void main(String[] args) {
         System.out.println("Did it update");
+        System.out.println("Did it update");
+        System.out.println("Did it update");
+        System.out.println("Did it update");
+        System.out.println("Did it update");
 
     }
 }
